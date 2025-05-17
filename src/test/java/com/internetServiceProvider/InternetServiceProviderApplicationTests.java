500d4b1319c325eff330d2a48d214d0b37332be3
Kalavena Dharaneshwar <dharaneshwarkalavena@gmail.com>
2025-05-17 23:36:57 +0530

    First Commit

---
+++ src/test/java/com/internetServiceProvider/InternetServiceProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internetServiceProvider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetServiceProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
